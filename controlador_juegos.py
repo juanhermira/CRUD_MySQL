ce83df5b328b2456d893bbe337f94bbb0d8fc2e9
Juan Hermira Díaz <110692379+juanhermira@users.noreply.github.com>
2022-12-29 12:59:51 +0100

    Add files via upload

---
+++ controlador_juegos.py
@@ -0,0 +1,46 @@
+from bd import obtener_conexion
+
+def insertar_parametro(idParametro, valorParametro):
+    conexion = obtener_conexion()
+    with conexion.cursor() as cursor:
+        cursor.execute("INSERT INTO parametros(idParametro, valorParametro) VALUES (%s, %s)",
+                       (idParametro, valorParametro))
+    conexion.commit()
+    conexion.close()
+
+def obtener_parametros():
+    conexion = obtener_conexion()
+    parametros = []
+    with conexion.cursor() as cursor:
+        cursor.execute("SELECT idParametro, valorParametro FROM parametros")
+        parametros = cursor.fetchall()
+    conexion.close()
+    return parametros
+
+
+def eliminar_parametro(idParametro):
+    conexion = obtener_conexion()
+    with conexion.cursor() as cursor:
+        cursor.execute("DELETE FROM parametros WHERE idParametro = %s", (idParametro,))
+    conexion.commit()
+    conexion.close()
+
+
+def obtener_parametro_por_id(idParametro):
+    conexion = obtener_conexion()
+    parametro = None
+    with conexion.cursor() as cursor:
+        cursor.execute(
+            "SELECT idParametro, valorParametro FROM parametros WHERE idParametro = %s", (idParametro,))
+        parametro = cursor.fetchone()
+    conexion.close()
+    return parametro
+
+
+def actualizar_parametro(valorParametro, idParametro):
+    conexion = obtener_conexion()
+    with conexion.cursor() as cursor:
+        cursor.execute("UPDATE parametros SET valorParametro = %s WHERE idParametro = %s",
+                       (valorParametro, idParametro))
+    conexion.commit()
+    conexion.close()
---
+++ main.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request, redirect
+import controlador_juegos
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route("/agregar_parametro")
+def formulario_agregar_parametro():
+    return render_template("agregar_juego.html")
+
+
+@app.route("/guardar_parametro", methods=["POST"])
+def guardar_parametro():
+    idParametro = request.form["idParametro"]
+    valorParametro = request.form["valorParametro"]
+    controlador_juegos.insertar_parametro(idParametro, valorParametro)
+    # De cualquier modo, y si todo fue bien, redireccionar
+    return redirect("/parametros")
+
+
+@app.route("/")
+@app.route("/parametros")
+def parametros():
+    parametros = controlador_juegos.obtener_parametros()
+    return render_template("juegos.html", parametros=parametros)
+
+
+@app.route("/eliminar_parametro", methods=["POST"])
+def eliminar_parametro():
+    controlador_juegos.eliminar_parametro(request.form["idPatrametro"])
+    return redirect("/parametros")
+
+
+@app.route("/formulario_editar_parametro/<int:idParametro>")
+def editar_parametro(idParametro):
+    # Obtener el parametro por ID
+    parametro = controlador_juegos.obtener_parametro_por_id(idParametro)
+    return render_template("editar_juego.html", parametros=parametros)
+
+
+@app.route("/actualizar_parametro", methods=["POST"])
+def actualizar_parametro():
+    idParametro = request.form["idParametro"]
+    valorParametro = request.form["valorParametro"]
+    controlador_juegos.actualizar_parametro(idParametro, valorParametro)
+    return redirect("/parametros")
+
+
+# Iniciar el servidor
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000, debug=True)
